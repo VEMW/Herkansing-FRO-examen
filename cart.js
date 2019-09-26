609734c7f0133e251444a4f6e59963e6cc4472d5
VEMW <34155399+VEMW@users.noreply.github.com>
2019-09-26 22:11:20 +0200

    Update cart.js

--- cart.js
+++ cart.js
@@ -70,7 +70,7 @@ function addItemToCart(title, price, imageSrc) {
         <span class="cart-price cart-column">${price}</span>
         <div class="cart-quantity cart-column">
             <input class="cart-quantity-input" type="number" value="1">
-            <button class="btn btn-danger" type="button">REMOVE</button>
+            <button class="btn btn-danger" type="button">Verwijder</button>
         </div>`
     cartRow.innerHTML = cartRowContents
     cartItems.append(cartRow)
